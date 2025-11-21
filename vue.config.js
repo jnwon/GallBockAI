75d740db0beb8fd9e41e99f81063264233cee2b0
jnwon <atalanta16@naver.com>
2025-11-21 12:43:58 +0900

    setting for github page custom url

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,26 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, 'src'),
+			},
+		},
+		plugins: [
+			new CopyWebpackPlugin({
+				patterns: [
+					{
+						from: path.resolve(__dirname, 'CNAME'),
+						to: path.resolve(__dirname, 'dist'),
+					},
+				],
+			}),
+		],
+	},
   publicPath: '/GallBockAI',
   outputDir: 'docs'
 })
